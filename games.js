f4c0a78609898b41de93054987091a703225b545
Tom <vacekto@gmail.com>
2020-02-07 07:45:36 +0100

    first commit

---
+++ games.js
@@ -0,0 +1,46 @@
+const games = [];
+exports.games = games;  
+
+exports.getGame = (id) => {
+  return games.find(g => Object.values(g).includes(id));
+}
+
+exports.createGame = (id) => {
+  if (!games.some( g => g.host === id)){
+    const game = {
+      host: id,
+      guest: null
+    }
+    games.push(game);
+    setTimeout(() => {
+      const index = games.findIndex(g => g == game)
+      if (index !== -1 && !game.guest ){
+        games.splice(index,1)
+      }
+    }, 120000);
+  }
+}
+
+exports.deleteGame = (id) => {
+  const index = games.findIndex(g => Object.values(g).includes(id));
+  games.splice(index,1)
+}
+
+exports.joinGame = (hostId, guestId) => {
+  const game = games.find(g => g.host === hostId)
+  if(game){
+    game.guest=guestId; 
+    const index = games.findIndex(g => g.host === guestId);
+    if (index !== -1){
+      games.splice(index,1)
+    }
+  } else {
+    
+  }
+}
+
+exports.getRoom = (rooms, id) => {
+  return Object.keys(rooms).reduce((initial, current) => {
+    return (initial === id) ? current : initial
+  })
+}
---
+++ index.js
@@ -0,0 +1,59 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const cors =require("cors");
+
+const {getGame, createGame, deleteGame, joinGame, getRoom} = require("./games.js")
+ 
+const PORT = 3001;
+
+app.use(cors());
+
+io.on('connection', function (socket) {
+  socket.on("createGame", () => {
+      createGame(socket.id);
+    })    
+  
+
+  socket.on("joinGame", id => {
+    const game =getGame(id);
+    if (game && socket.id !== game.host){     
+      joinGame(id, socket.id)
+      socket.join(id);
+      io.to(id).emit("startGame");
+    } else {
+      socket.emit("invsalid input");      
+    }
+  }) 
+  
+  socket.on("update", data => {
+    const room = getRoom(socket.rooms, socket.id);
+    socket.to(room).emit("update", data);
+  })
+
+  socket.on("I won", (data) => {
+    const room = getRoom(socket.rooms, socket.id);
+    socket.to(room).emit("opponent won", data);
+  })
+
+  socket.on("play again", () => {
+    const room = getRoom(socket.rooms, socket.id);
+    socket.to(room).emit("play again");
+  })
+
+  socket.on("opponent left", () => {
+    const room = getRoom(socket.rooms, socket.id);
+    socket.to(room).emit("opponent left");
+    deleteGame(socket.id);
+  })
+
+  socket.on("disconnect", () => {
+    if (getGame(socket.id)){
+      io.in(getGame(socket.id).host).emit("opponent left");
+      deleteGame(socket.id);
+    }
+  })
+});
+
+
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
